a8520f1a2a1812a380ba1e0681ab82e628bae67e
Steve Cai <steve.cai@morningstar.com>
2015-07-02 16:55:53 +0800

    finish the UI

---
+++ translator.js
@@ -0,0 +1,7 @@
+﻿function Translate(eng) {
+    return "你是个大傻逼";
+}
+
+function Read(chn) {
+    return "http://tsn.baidu.com/text2audio?tex="+chn+"&lan=zh&cuid=aaaascasc&ctp=1&tok=24.3ca118e2b9bb3fe63885ed35f97b4473.2592000.1438357067.282335-6345082";
+}
